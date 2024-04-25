f1528d5f3f66680b5cd45607f5072163542ce61c
Nayaka Yoga Pradipta <nayakayp@gmail.com>
2024-04-25 12:22:42 +0700

    feat: add announcement

---
+++ src/components/Announcement.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Announcement = (props: Props) => {
+  return (
+    <div className="fixed z-50 top-0 left-0 right-0 w-full bg-yellow-500 text-white font-noto-sans-display text-sm text-center font-medium py-2">
+      <p>This page is a special offer for BHW member</p>
+    </div>
+  );
+};
+
+export default Announcement;
--- src/pages/special-bhw.tsx
+++ src/pages/special-bhw.tsx
@@ -6,6 +6,7 @@ import PricingSection from "@/components/bhw/PricingSection";
 import FaqSection from "@/components/bhw/FaqSection";
 import Footer from "@/components/bhw/Footer";
 import Head from "next/head";
+import Announcement from "@/components/Announcement";
 
 export default function SpecialBHW() {
   return (
@@ -13,6 +14,7 @@ export default function SpecialBHW() {
       <Head>
         <title>WPEnigma – Unlimited Wordpress Creation Service</title>
       </Head>
+      <Announcement />
       <HeaderSection />
       <FacilitySection />
       <WeWorkFastSection />
