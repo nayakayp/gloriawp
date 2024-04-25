1285761342815babf1cb25fd65d44df6c5b9ba44
Nayaka Yoga Pradipta <nayakayp@gmail.com>
2024-04-25 15:52:15 +0700

    feat: add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
+import { Fragment } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Fragment>
+      <Component {...pageProps} />;
+      <GoogleAnalytics gaId="G-BG7M8ZY4EG" />
+      <GoogleTagManager gtmId="BG7M8ZY4EG" />
+    </Fragment>
+  );
 }
