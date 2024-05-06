3d543d0698bf23d6a8c7a937b97ef095547676a4
Nayaka Yoga Pradipta <nayakayp@gmail.com>
2024-05-06 22:07:18 +0700

    fix: announcement and payment method style

--- src/components/Announcement.tsx
+++ src/components/Announcement.tsx
@@ -4,7 +4,7 @@ type Props = {};
 
 const Announcement = (props: Props) => {
   return (
-    <div className="fixed z-50 top-0 left-0 right-0 w-full bg-yellow-500 text-white font-noto-sans-display text-sm text-center font-medium py-2">
+    <div className="fixed z-50 top-0 left-0 right-0 w-full bg-yellow-500 text-white font-noto-sans-display text-sm text-center font-medium py-1">
       <p>This page is a special offer for BHW member</p>
     </div>
   );
